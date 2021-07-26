0bc9860a4c511ba414cec4920096bdeebc318ce9
Techno-Simpola <ashukumarsinghrathore123@gmail.com>
2021-07-26 19:18:39 +0530

    Overall UI change of lanfing page

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -2,7 +2,7 @@ const initBg = (autoplay = true) => {
     const bgImgsNames = ['boy-1.jpg','girl4.jpg', 'boy3-2.jpg'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
-    $.backstretch(bgImgs, {duration: 2500, fade: 900});
+    $('.tm-container').backstretch(bgImgs, {duration: 2500, fade: 900});
 
     if(!autoplay) {
       $.backstretch('pause');  
@@ -53,3 +53,34 @@ $(document).ready(function () {
         setBgOverlay();
     });
 });
+
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.querySelector(".up-button");
+const downButton = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+  if (direction === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) activeSlideIndex = 0;
+  } else if (direction === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) activeSlideIndex = slidesLength - 1;
+  }
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
+
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
